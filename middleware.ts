07b068c72589a93da444471d3b0914e41d1a42d6
Ullegadda Saisrikanta <ullegadda.srikanta@gmail.com>
2025-02-14 00:48:31 +0530

    Update middleware to redirect authenticated users from auth pages

--- middleware.ts
+++ middleware.ts
@@ -8,10 +8,17 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/auth/signin", request.url))
   }
 
+  if (
+    jwt &&
+    (request.nextUrl.pathname.startsWith("/auth/signin") || request.nextUrl.pathname.startsWith("/auth/signup"))
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url))
+  }
+
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: "/dashboard/:path*",
+  matcher: ["/dashboard/:path*", "/auth/signin", "/auth/signup"],
 }
 
